4528018471b5108928e8914edde98eb6df733668
cjoraeki <cjoraeki3@gmail.com>
2023-03-25 20:29:19 +0100

    error validation

--- src/pages/changes/UpdateUsername.jsx
+++ src/pages/changes/UpdateUsername.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { updateAData } from "../../utils/datalayer";
 import "../forgot-password/forget.css";
 import { NavBar } from "../../components/nav/NavBar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const UpdateUsername = () => {
   const navigate = useNavigate();
@@ -16,16 +18,16 @@ export const UpdateUsername = () => {
     e.preventDefault();
     console.log(formData);
 
-    // formData["emailForOTP"] = "cjoraeki3@gmail.com"; //find how to fetch
     const res = await updateAData("update-profile", formData);
     const { status, error, data } = res;
-
     console.log(status, error, data);
 
     if (status === 200) {
-      return navigate("/profile");
-    } else if (status === 400) {
-      return alert("Error. Please try again ");
+      localStorage.setItem("user", JSON.stringify(data));
+      toast.success("Username changed successfully");
+      return setTimeout(() => {
+        navigate("/profile");
+      }, 3000);
     } else {
       alert("Error changing your username");
       return navigate("/settings");
@@ -51,18 +53,13 @@ export const UpdateUsername = () => {
               name="username"
               required
             />
-            {/* <input
-              type="text"
-              placeholder="Confirm Username"
-              onChange={handleChange}
-              name="username"
-            /> */}
             <button type="submit" className="btn btn-wide">
               Submit
             </button>
           </form>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
